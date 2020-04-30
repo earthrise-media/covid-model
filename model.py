90594b9eef5cf2984220beecd32ce45fd3a90d60
danhammer <dan.s.hammer@gmail.com>
2020-04-29 21:05:12 -0700

    finished a full outline of the new, simplified exposition

--- model.py
+++ model.py
@@ -39,9 +39,9 @@ In the case that the 2 cohort (indexing from 0) is not mixing but all others
 are, and BETA_CONST=.2, 
 
 beta = np.array([[0.2, 0.2, 0.0, 0.2],
-                 [0.2, 0.2, 0.0, 0.2],
-                 [0.0, 0.0, 0.0, 0.0],
-                 [0.2, 0.2, 0.0, 0.2]])
+				 [0.2, 0.2, 0.0, 0.2],
+				 [0.0, 0.0, 0.0, 0.0],
+				 [0.2, 0.2, 0.0, 0.2]])
 
 Of course this restricted use of the transmission matrix leaves considerable 
 room for more nuanced modeling.
@@ -56,134 +56,109 @@ import scipy.integrate
 
 COMPARTMENTS = ['Susceptible', 'Exposed', 'Infected', 'Died or recovered']
 
-# Example age-cohort structure and initial populations
-COHORTS = ['0-18', '19-34', '35-64', '65+']
-ROUGH_2017_POPULATION = [76., 68., 144., 52.]  # in millions, per Wikipedia 
-POPULATION_FRACTIONS = ROUGH_2017_POPULATION / np.sum(ROUGH_2017_POPULATION)
-initial_infected = .002
-pop_0 = np.round(
-    np.array([
-        [f - initial_infected, 0, initial_infected, 0] for f in POPULATION_FRACTIONS
-    ]), 
-    decimals=5
-)
-
-# Model parameters:
-INCUBATION_PERIOD = 5
-DURATION_OF_INFECTION = 14
-MIXING_BETA_CONST = .4
-BETA_ALL_COHORTS_MIXING = MIXING_BETA_CONST * np.ones((len(COHORTS), len(COHORTS)))
-DEATH_RATES_BY_COHORT = [0.001, 0.002, 0.004, 0.01]
-
 class SEIRModel(object):
-    """Class to solve an age-structured SEIR Compartmental model.
+	"""Class to solve an age-structured SEIR Compartmental model.
 
-    Attributes: 
-        betas: List of transmission matrices, one per epoch. (An epoch here
-            denotes a period in which transmission rates are constant.) 
-        epoch_end_times: List of days at which transmission rates change.
-        alpha: Inverse incubation period
-        gamma: Inverse duration of infection
-        cohorts: List of names of (age) cohorts
-        N_cohorts: Number of cohorts
-        compartments: List of names of compartments
-        N_compartments: Number of compartments (hard-coded to 4: S-E-I-R)
-        s, e, i, r: Indices of the compartments in the state vector y(t). 
+	Attributes: 
+		betas: List of transmission matrices, one per epoch. (An epoch here
+			denotes a period in which transmission rates are constant.) 
+		epoch_end_times: List of days at which transmission rates change.
+		alpha: Inverse incubation period
+		gamma: Inverse duration of infection
+		cohorts: List of names of (age) cohorts
+		N_cohorts: Number of cohorts
+		compartments: List of names of compartments
+		N_compartments: Number of compartments (hard-coded to 4: S-E-I-R)
+		s, e, i, r: Indices of the compartments in the state vector y(t). 
 
-    External methods: 
-        f: Function giving the rate of change in the state variable y(t).
-        solve: Integrate the coupled differential equations.
-        solve_to_dataframe: Solve and output a tidy dataframe.
-    """
+	External methods: 
+		f: Function giving the rate of change in the state variable y(t).
+		solve: Integrate the coupled differential equations.
+		solve_to_dataframe: Solve and output a tidy dataframe.
+	"""
 
-    def __init__(
-            self, betas, epoch_end_times, 
-            alpha=1/INCUBATION_PERIOD,
-            gamma=1/DURATION_OF_INFECTION, 
-            cohorts=COHORTS
-        ):
-        
-        if len(epoch_end_times) != len(betas):
-            raise ValueError('Each beta matrix requires an epoch end time.')
-        self.epoch_end_times = sorted(epoch_end_times)
-        self.betas = betas
-        self.alpha = alpha
-        self.gamma = gamma
-        self.cohorts = cohorts
-        self.N_cohorts = len(cohorts)
-        
-        # N.B. hard-coded values. The function f() assumes these four
-        # compartments.
-        self.compartments = COMPARTMENTS
-        self.N_compartments = 4
-        self.s, self.e, self.i, self.r = list(range(4))                  
-                                                       
-    def _beta(self, t):
-        """Fetch the beta matrix for given time t."""
-        for end_time, beta in zip(self.epoch_end_times, self.betas): 
-            if t <= end_time: 
-                return beta      
-        
-    def f(self, t, y):
-        """Function giving the rate of change in the state variable y(t)."""
-        y = y.reshape(self.N_cohorts, self.N_compartments)
-        dy = np.zeros((self.N_cohorts, self.N_compartments))
-        beta = self._beta(t)
-        for a in range(self.N_cohorts):
-            infection_rate = np.sum([beta[a,b] * y[b,self.i] / np.sum(y[b,:])
-                                         for b in range(self.N_cohorts)])
-            dy[a,self.s] = -y[a,self.s] * infection_rate
-            dy[a,self.e] = (y[a,self.s] * infection_rate
-                                - self.alpha * y[a, self.e])
-            dy[a,self.i] = self.alpha * y[a, self.e] - self.gamma * y[a, self.i]
-            dy[a,self.r] = self.gamma * y[a, self.i]
-        return dy.flatten()
-    
-    def solve(self, y0):
-        """Integrate the coupled differential equations."""
-        sol = scipy.integrate.solve_ivp(
-            self.f, (0, self.epoch_end_times[-1]), y0, 
-            t_eval=np.arange(self.epoch_end_times[-1]))
-        return sol.t, sol.y
-    
-    def solve_to_dataframe(self, y0, detailed_output=False):
-        """Solve and output a tidy dataframe."""
-        t, y = self.solve(y0)
-        y = y.reshape(self.N_cohorts, self.N_compartments, len(t))
+	def __init__(
+			self, betas, epoch_end_times, 
+			incubation_period=5,
+			duration_of_infection=14,
+			cohorts=['0-18', '19-34', '35-64', '65+']
+		):
+		
+		if len(epoch_end_times) != len(betas):
+			raise ValueError('Each beta matrix requires an epoch end time.')
+		self.epoch_end_times = sorted(epoch_end_times)
+		self.betas = betas
+		self.alpha = 1/incubation_period
+		self.gamma = 1/duration_of_infection
+		self.cohorts = cohorts
+		self.N_cohorts = len(cohorts)
+		
+		# N.B. hard-coded values. The function f() assumes these four
+		# compartments.
+		self.compartments = COMPARTMENTS
+		self.N_compartments = 4
+		self.s, self.e, self.i, self.r = list(range(4))                  
+													   
+	def _beta(self, t):
+		"""Fetch the beta matrix for given time t."""
+		for end_time, beta in zip(self.epoch_end_times, self.betas): 
+			if t <= end_time: 
+				return beta      
+		
+	def f(self, t, y):
+		"""Function giving the rate of change in the state variable y(t)."""
+		y = y.reshape(self.N_cohorts, self.N_compartments)
+		dy = np.zeros((self.N_cohorts, self.N_compartments))
+		beta = self._beta(t)
+		for a in range(self.N_cohorts):
+			infection_rate = np.sum([beta[a,b] * y[b,self.i] / np.sum(y[b,:])
+										 for b in range(self.N_cohorts)])
+			dy[a,self.s] = -y[a,self.s] * infection_rate
+			dy[a,self.e] = (y[a,self.s] * infection_rate
+								- self.alpha * y[a, self.e])
+			dy[a,self.i] = self.alpha * y[a, self.e] - self.gamma * y[a, self.i]
+			dy[a,self.r] = self.gamma * y[a, self.i]
+		return dy.flatten()
+	
+	def solve(self, y0):
+		"""Integrate the coupled differential equations."""
+		sol = scipy.integrate.solve_ivp(
+			self.f, (0, self.epoch_end_times[-1]), y0, 
+			t_eval=np.arange(self.epoch_end_times[-1]))
+		return sol.t, sol.y
+	
+	def solve_to_dataframe(self, y0, detailed_output=False):
+		"""Solve and output a tidy dataframe."""
+		t, y = self.solve(y0)
+		y = y.reshape(self.N_cohorts, self.N_compartments, len(t))
 
-        # calculate the time series for the total population
-        aggregate_nums = np.sum(y, axis=0)
-        df = pd.DataFrame(dict({'days': t}, **dict(zip(self.compartments, aggregate_nums))))
-        df = pd.melt(df, id_vars=['days'], var_name='Group', value_name='pop')
-        df['Date(s) of intervention'] = str(self.epoch_end_times[:-1])
+		# calculate the time series for the total population
+		aggregate_nums = np.sum(y, axis=0)
+		df = pd.DataFrame(dict({'days': t}, **dict(zip(self.compartments, aggregate_nums))))
+		df = pd.melt(df, id_vars=['days'], var_name='Group', value_name='pop')
+		df['Date(s) of intervention'] = str(self.epoch_end_times[:-1])
 
-        # Calculate the aggregate deaths by cohort.  Position matters!!!  The
-        # "Died or recovered" compartment is in the final position. Then,
-        # also, collect the final number in the evolution, hence the dual "-1"
-        # indices.
-        final_died_or_recovered = [x[-1][-1] for x in y]
-        number_died_or_recovered = np.multiply(final_died_or_recovered, ROUGH_2017_POPULATION)
-        death_numbers = np.multiply(number_died_or_recovered, DEATH_RATES_BY_COHORT)
-
-        death_df = pd.DataFrame([death_numbers], columns = COHORTS, index=["Deaths (millions)"])
-        death_df["Total"] = sum(death_numbers)
-
-        if detailed_output == True:
-            return df, death_df, y
-        else:
-            return df, death_df
+		if detailed_output == True:
+			return df, y
+		else:
+			return df
 
 
-def model_input(sequestered_ranges, seclusion_scale=0.02, evolution_length=180):
+def model_input(sequestered_ranges, 
+		seclusion_scale=0.02,
+		mixing_beta_const=0.4, 
+		evolution_length=180,
+		cohorts=['0-18', '19-34', '35-64', '65+']
+	):
 	"""
 	Function to calculuate transmission matrices and to enumerate their end
 	period.
 
 	Argument exclusion_ranges: List of day range tuples denoting the period
-        during which each cohort is excluded from the general population.
+		during which each cohort is excluded from the general population.
 
-    Argument seclusion_scale:  There will always be *some* mixing, even under
-        shelter-in-place
+	Argument seclusion_scale:  There will always be *some* mixing, even under
+		shelter-in-place
 
 	Returns a list of two lists for input into SEIRModel():
 		1. A list of the transition matrices
@@ -219,13 +194,15 @@ def model_input(sequestered_ranges, seclusion_scale=0.02, evolution_length=180):
 			return False
 
 	transition_matrices = []
+	
+	beta_all_cohorts_mixing = mixing_beta_const * np.ones((len(cohorts), len(cohorts)))
 	# iteratively alter the BETA_ALL_COHORTS_SEQUESTERED matrix by checking if a
-    # given population should be added for each time partition
+	# given population should be added for each time partition
 	for j in range(len(epoch_tuples)): 
 
 		# reset general popultation matrix for each epoch, and set
-        # transmission to zero values for cohorts that have been removed
-		res_mat = BETA_ALL_COHORTS_MIXING.copy()
+		# transmission to zero values for cohorts that have been removed
+		res_mat = beta_all_cohorts_mixing.copy()
 
 		for i in range(len(sequestered_ranges)):
 			if _intersects(sequestered_ranges[i], epoch_tuples[j]):
--- simpleapp.py
+++ simpleapp.py
@@ -17,6 +17,22 @@ region_option = st.sidebar.selectbox(
     ["North America", "Africa", "Europe"]
 )
 
+start_date = st.sidebar.number_input(
+    label='Start day (for visualization)',
+    value=int(),
+    min_value=int(0),
+    max_value=int(60),
+    step=int()
+)
+
+initial_infected = st.sidebar.number_input(
+    label='Initial rate of infection',
+    min_value=0.01,
+    max_value=0.7,
+    step=0.01
+)
+
+
 
 pop_lookup = {
     "North America": [90., 77., 140., 62.],
@@ -27,6 +43,14 @@ pop_lookup = {
 cohort_ages = ['0-18', '19-34', '35-64', '65+']
 population = pop_lookup[region_option]
 pop_fractions = population / np.sum(population)
+
+pop_0 = np.array([
+    [f - initial_infected, 0, initial_infected, 0] for f in pop_fractions
+])
+
+# Beta entries for mixing in the population and shelter-in-place
+MIXING = 0.4
+SECLUSION = 0.04
 
 # Visualization specification (default)
 
@@ -137,408 +161,149 @@ st.write(df)
 
 st.write("""
 
-The model is run . 
+Based on this information and **the parameter selections in the sidebar** the
+initial conditions are as follows:
 
-""")
+Initial conditions for **%s** (2020) as a proportion of the population:
+
+""" % region_option)
+
+display_pop_0 = pd.DataFrame(
+    pop_0,
+    index=cohort_ages,
+    columns=model.COMPARTMENTS
+)
+
+st.write(display_pop_0)
+
+st.write("""
+
+If an age cohort is not mixing, then its transmission rate between any other
+cohort is assumed to be **%s**. If the cohort is mixing when, for example, the
+NPI is lifted, then the transmission rate is **%s**. These entries represent
+the cell values in the model's *&beta;* matrix. 
+
+
+"""
+
+% (SECLUSION, MIXING)
+
+)
 
 st.subheader('An illustration of *Flattening the Curve*')
 
 st.write("""
 
 The simplest version of the model is a constant transmission matrix.  This is
-effectively the standard SEIR model, which has been widelyl used to show the
+effectively the standard SEIR model, which has been widely used to show the
 benefits of acting early in a pandemic to 'flatten the curve.' 
+
+Note that the region will not make a difference for the infection curve for
+this illustration, since the age distribution does not matter &mdash; all ages
+have been collapsed into a single, total population.
 
 """)
 
 mixing_range = st.slider(
     'Period of shelter-in-place for whole population',
-    0, 300, (80, 140)
+    0, 300, (0, 160)
 )
 
 cohort_ranges = np.repeat([mixing_range], 4, axis=0)
 
 betas, epoch_end_times = model.model_input(
     cohort_ranges, 
-    seclusion_scale=0.001
+    seclusion_scale=SECLUSION,
+    evolution_length=300,
+    mixing_beta_const=MIXING
 )
 
-# initial infected is 0.01% of the population
-pop_0 = np.array([
-    [f - 0.0001, 0, 0.0001, 0] for f in pop_fractions
-])
-
-st.write(pop_0)
-
-# res = model.SEIRModel(betas=betas, epoch_end_times=epoch_end_times)
-# df = res.solve_to_dataframe(model.pop_0.flatten())
-
-# st.vega_lite_chart(x`
-#   data=df[df["Group"] == "Infected"],
-#     spec=_vega_default_spec(
-#       color="#e45756",
-#       scale=[0.0, 0.6]
-#     ),
-#   use_container_width=True
-# )
-
-
-
-# st.sidebar.markdown("-------")
-
-# # Create a series of sliders for the time range of each cohort
-# # TODO: There is probably a more elegant way to do this.
-# first_range = st.slider(
-# 	'Period of NPI for [%s] cohort:' % (model.COHORTS[0]),
-# 	0, 180, (115, 180)
-# )
-
-# second_range = st.slider(
-# 	'Period of NPI for [%s] cohort:' % (model.COHORTS[1]),
-# 	0, 180, (80, 180)
-# )
-
-# third_range = st.slider(
-# 	'Period of NPI for [%s] cohort:' % (model.COHORTS[2]),
-# 	0, 180, (10, 80)
-# )
-
-# fourth_range = st.slider(
-# 	'Period of NPI for [%s] cohort:' % (model.COHORTS[3]),
-# 	0, 180, (0, 115)
-# )
-
-# cohort_ranges = [
-# 	first_range,
-# 	second_range,
-# 	third_range,
-# 	fourth_range
-# ]
-
-
-# # Generate the beta matrices and epoch ends:
-# betas, epoch_end_times = model.model_input(
-# 	cohort_ranges, 
-# 	seclusion_scale=0.001
-# )
-
-# res = model.SEIRModel(betas=betas, epoch_end_times=epoch_end_times)
-# df, death_df = res.solve_to_dataframe(model.pop_0.flatten())
-
-# colors = dict(zip(model.COMPARTMENTS, ["#4c78a8", "#f58518", "#e45756", "#72b7b2"]))
-
-# def _vega_default_spec(
-#         color=None, 
-#         scale=[0.0, 1.0], 
-#         evolution_length=180,
-#         start_day=0
-#     ):
-#     spec={
-#         'mark': {'type': 'line', 'tooltip': True},
-#         'encoding': {
-#             'x': {
-#                 'field': 'days', 
-#                 'type': 'quantitative',
-#                 'axis': {'title': ""},
-#                 'scale': {'domain': [start_day, evolution_length]}
-#             },
-#             'y': {
-#                 'field': 'pop', 
-#                 'type': 'quantitative',
-#                 'axis': {'title': ""},
-#                 'scale': {'domain': scale}
-#             },
-#         	'color': {'field': 'Group', 'type': 'nominal'}
-#         }
-#     }
-#     if color:
-#         spec['encoding'].pop('color')
-#         spec['mark'].update({'color': color})
-#     return spec
-
-# if show_option == "All":
-#     st.subheader(show_option)
-#     st.vega_lite_chart(
-#         data=df,
-#         spec=_vega_default_spec(),
-#         use_container_width=True
-#     )
-    
-# elif show_option == "Infected":
-# 	st.subheader(show_option)
-# 	st.vega_lite_chart(
-# 		data=df[df["Group"] == show_option],
-#         spec=_vega_default_spec(
-#         	color=colors[show_option],
-#         	scale=[0.0, 0.6]
-#         ),
-# 		use_container_width=True
-# 	)
-
-# elif show_option in model.COMPARTMENTS:
-# 	st.subheader(show_option)
-# 	st.vega_lite_chart(
-# 		data=df[df["Group"] == show_option],
-#         spec=_vega_default_spec(color=colors[show_option]),
-# 		use_container_width=True
-# 	)
-
-# st.dataframe(death_df.style.highlight_max(axis=1, color="#e39696"))
-
-# st.markdown(""" 
-
-# The parameter values are **especially** wrong. We just made up some numbers. These
-# should be set from empirical evidence.
-
-# Note that there are _a lot_ of free parameters.  It is possible to create
-# basically any model outcome from the choice of these interdependent
-# parameters.  This web app is valuable insofar as it illustrates the broad
-# trends and features of the standard compartmental models, when interacting
-# cohorts are introduced.  For example, there can be two or even more waves of
-# infection.  That's like, basically, that's about all we can get from this.
-
-# """)
-
-
-# # Policy scenarios through illustration of the behavior of differential
-# # equations.
-
-# st.subheader('Flattening the curve, illustrated.')
-
-# st.write(""" 
-
-# The concept of *flattening the curve* is now well-understood. If you delay the
-# start of the NPI, and allow mixing for the early period, you can observe the
-# spike in infection. (This corresponds to moving the start period of the NPI
-# from Day 0 to, say, Day 20.)  This further underscores the value of immediate
-# action in a pandemic. The curve remains flat if the NPI is enacted, with only
-# a small bump in infections when the NPI is removed.  
-
-# """)
-
-# mixing_range = st.slider(
-# 	'Period of intervention for whole population',
-# 	0, 180, (0, 90)
-# )
-
-# # All cohorts mix at the same time.
-# cohort_ranges = np.repeat([mixing_range], 4, axis=0)
-# betas, epoch_end_times = model.model_input(cohort_ranges, seclusion_scale=0.05)
-
-# res = model.SEIRModel(betas=betas, epoch_end_times=epoch_end_times)
-# df, death_df = res.solve_to_dataframe(model.pop_0.flatten())
-
-# st.vega_lite_chart(
-# 	data=df[df["Group"] == "Infected"],
-#     spec=_vega_default_spec(
-#     	color="#e45756",
-#     	scale=[0.0, 0.6]
-#     ),
-# 	use_container_width=True
-# )
-
-# st.write(death_df)
-
-# st.subheader('Different NPIs for different sub-populations.')
-
-# st.markdown(""" 
-
-# What happens if we allow some people to re-enter the economy before others?
-# For example, suppose we allow young people to go to school before allowing
-# *everyone* to mix. The infection rate won't necessarily change, but the
-# hospitalizations may not spike as high, since younger people don't get so
-# sick.
-
-# Consider a made-up country with a very young population &mdash; with 75% of
-# the population under a certain age, say, under the age of 35.  Assume further
-# that the severity of the illness is much worse in older people, requiring 
-# hospitalization at a much greater rate than for young people that contract the
-# illness.  Returning to business as usual requires hospitalization to remain
-# under a certain threshold.
-
-# The following graph illustrates the concept of phased re-introduction of two
-# subpopulations with different hospitalization usage.  The parameters are made
-# up to clearly show the unique features of this cohort model.  Specifically,
-# this model illustrates NPIs with differential treatment of sub-populations
-# with different characteristics, but that interact with the evolution of the
-# illness in all other sub-populations.
-
-# Our suggestion is to play with the sliders to introduce the older population
-# earlier, noting when the severe infection rate (in orange) exceeds an
-# arbitrary threshold (like 0.1).
-
-# """)
-
-# population = [50, 20, 20, 10]  # in millions
-# population_fractions = population / np.sum(population)
-# initial_infected = 0.002
-
-# # Create an initial population array, with 4 (S, E, I, and R) compartments
-# pop_0 = np.round(
-#     np.array([
-#         [f - initial_infected, 0, initial_infected, 0] 
-#         for f in population_fractions
-#     ]), 
-#     decimals=5
-# )
-
-# young_range = st.slider(
-# 	'Period of NPI for younger cohort:',
-# 	0, 100, (0, 16)
-# )
-
-# old_range = st.slider(
-# 	'Period of NPI for older cohort:',
-# 	0, 100, (0, 60)
-# )
-
-# cohort_ranges = [young_range, young_range, old_range, old_range]
-
-# betas, epoch_end_times = model.model_input(
-#     cohort_ranges, 
-#     seclusion_scale=0.03,
-#     evolution_length=130
-# )
-
-# res = model.SEIRModel(betas=betas, epoch_end_times=epoch_end_times)
-# df, death_df, y = res.solve_to_dataframe(pop_0.flatten(), detailed_output=True)
-
-# young = pd.DataFrame(
-#     {
-#         "infection1": y[0][2],
-#         "infection2": y[1][2]
-#     }
-# )
-
-# old = pd.DataFrame(
-#     {
-#         "infection1": y[2][2],
-#         "infection2": y[3][2]
-#     }
-# )
-
-# df = pd.DataFrame(
-# 	{
-# 		"infection": young["infection1"] + young["infection2"] + old["infection1"] + old["infection2"],
-# 		"severe": (young["infection1"] + young["infection2"]) * 0.15 + (old["infection1"] + old["infection2"]) * 0.85
-# 	}
-# )
-
-# st.area_chart(df)
-
-# ## STACKING NPIs with regional adjustment
-
-# st.sidebar.markdown(
-#     """
-#     **Illustration #4**: Stacking NPIs with regional adjustment
-
-#     """
-# )
-
-# region_option = st.sidebar.selectbox(
-#     'Region', 
-#     ["North America", "Africa", "Europe"]
-# )
-
-# start_date = st.sidebar.number_input(
-#     label='Start day',
-#     value=int(),
-#     min_value=int(0),
-#     max_value=int(60),
-#     step=int()
-# )
-
-# st.sidebar.markdown("-------")
-
-# st.subheader('Stacking NPIs: An illustration.')
-
-# st.markdown(""" 
-
-# It is challenging to distinguish the effect of individual public health and
-# social measures (PHSMs) on the rate of COVID-19 transmission within a
-# population (reproductive number R0); effectiveness depends on how fully
-# communities adopt and adhere to PHSMs, additional interventions they are
-# combined with, and other variables like family size and level of
-# intergenerational contact within a community. Still, evidence does show that
-# PHSMs are more effective when implemented in combination, or “stacked,” than
-# when implemented individually. 
-
-# The following scenario expressly illustrates this concept, when the PHSMs are
-# tied to partitions of the general population (i.e., a person can only belong
-# to one category). This is the primary limitation of this illustration, one
-# that can be mitigated with more precise definitions of NPIs. There is the
-# added feature of chosing the initial conditions based on region.  See the
-# sidebar for options.  The default is the United States.  
-
-# """)
-
-# pop_lookup = {
-#     "North America": [90., 77., 140., 62.],
-#     "Africa"       : [679., 316., 298., 47.],
-#     "Europe"       : [158., 136., 310., 142.]
-# }
-
-
-# cohort_ages = ['0-18', '19-34', '35-64', '65+']
-# pop_2020 = pop_lookup[region_option]
-# pop_fractions = pop_2020 / np.sum(pop_2020)
-
-
-# st.markdown('''
-#     > **%s** (2020) 
-#     >> *Use sidebar to change region. Data from UN.*
-# ''' % region_option)
-
-# initial_populations = np.round(
-#     np.array([
-#         [f - initial_infected, 0, initial_infected, 0] for f in pop_fractions
-#     ]), 
-#     decimals=5
-# )
-
-# first_npi = st.slider(
-#     'Schools closed.',
-#     0, 180, (0, 20)
-# )
-
-# second_npi = st.slider(
-#     'Restaurants, universities, and bars closed.',
-#     0, 180, (0, 60)
-# )
-
-# third_npi = st.slider(
-#     'Offices closed.',
-#     0, 180, (0, 80)
-# )
-
-# fourth_npi = st.slider(
-#     'Senior citizens remained quarantined.',
-#     0, 180, (0, 120)
-# )
-
-# npi_ranges = [
-#     first_npi,
-#     second_npi,
-#     third_npi,
-#     fourth_npi
-# ]
-
-# betas, epoch_end_times = model.model_input(
-#     npi_ranges, 
-#     seclusion_scale=0.02
-# )
-
-# res = model.SEIRModel(betas=betas, epoch_end_times=epoch_end_times)
-# df, death_df = res.solve_to_dataframe(initial_populations.flatten())
-# infected = df[df["Group"] == "Infected"]
-
-# st.vega_lite_chart(
-#     data=infected[infected["days"] > start_date],
-#     spec=_vega_default_spec(
-#         color="#e45756",
-#         scale=[0.0, 0.6],
-#         start_day=start_date
-#     ),
-#     use_container_width=True
-# )
 
+
+res = model.SEIRModel(betas=betas, epoch_end_times=epoch_end_times)
+df = res.solve_to_dataframe(pop_0.flatten())
+
+infected = df[df["Group"] == "Infected"]
+
+st.vega_lite_chart(
+  data=infected[infected["days"] > start_date],
+    spec=_vega_default_spec(
+      color="#e45756",
+      scale=[0.0, 0.3],
+      evolution_length=300
+    ),
+  use_container_width=True
+)
+
+st.subheader('Stacking NPIs: An illustration.')
+
+st.write("""
+
+It is challenging to distinguish the effect of individual public health and
+social measures (PHSMs) on the rate of COVID-19 transmission within a
+population (reproductive number R0); effectiveness depends on how fully
+communities adopt and adhere to PHSMs, additional interventions they are
+combined with, and other variables like family size and level of
+intergenerational contact within a community. Still, evidence does show that
+PHSMs are more effective when implemented in combination, or “stacked,” than
+when implemented individually. 
+
+The following scenario expressly illustrates this concept, when the PHSMs are
+tied to partitions of the general population (i.e., a person can only belong
+to one category). This is the primary limitation of this illustration, one
+that can be mitigated with more precise definitions of NPIs. There is the
+added feature of chosing the initial conditions based on region.  See the
+sidebar for options.  The default is the United States.  
+
+""")
+
+
+first_npi = st.slider(
+    'Schools closed.',
+    0, 180, (0, 20)
+)
+
+second_npi = st.slider(
+    'Restaurants, universities, and bars closed.',
+    0, 180, (0, 60)
+)
+
+third_npi = st.slider(
+    'Offices closed.',
+    0, 180, (0, 80)
+)
+
+fourth_npi = st.slider(
+    'Senior citizens remained quarantined.',
+    0, 180, (0, 120)
+)
+
+npi_ranges = [
+    first_npi,
+    second_npi,
+    third_npi,
+    fourth_npi
+]
+
+betas, epoch_end_times = model.model_input(
+    npi_ranges, 
+    seclusion_scale=SECLUSION,
+    evolution_length=300,
+    mixing_beta_const=MIXING
+)
+
+
+res = model.SEIRModel(betas=betas, epoch_end_times=epoch_end_times)
+df = res.solve_to_dataframe(pop_0.flatten())
+infected = df[df["Group"] == "Infected"]
+
+st.vega_lite_chart(
+    data=infected[infected["days"] > start_date],
+    spec=_vega_default_spec(
+        color="#e45756",
+        scale=[0.0, 0.3],
+        start_day=start_date,
+        evolution_length=300
+    ),
+    use_container_width=True
+)
